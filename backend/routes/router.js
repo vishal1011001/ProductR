7f2bd868cafdd0bd494cf8e0ee2f01bceec81d1b
vishal1011001 <vishaljakharbusinessmail@gmail.com>
2026-05-14 18:06:22 +0530

    fixed index.js bugs

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -3,12 +3,12 @@ import {retrieveP, postP, putP, removeP} from '../controllers/productController.
 
 const router = express.Router();
 
-router.get('/products', retrieveJob);
+router.get('/products', retrieveP);
 
-router.post('/products', postJob);
+router.post('/products', postP);
 
-router.put('/products/:id', putJob);
+router.put('/products/:id', putP);
 
-router.delete('/products/:id', removeJob);
+router.delete('/products/:id', removeP);
 
 export default router;
